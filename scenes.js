83d932e2f6248bc2ef3b9b31d6d093eaa308bbc4
Arvis Ceirulis <arvis@Arviss-MacBook-Pro.local>
2023-09-10 18:51:21 +0300

    feat: created a class system instead of a disgusting switch statement

--- scenes.js
+++ scenes.js
@@ -1,25 +1,22 @@
-function switch_scene(scene){
-    switch(scene){
-        case 0:
-            buttons = [];
-            buttons.push([1,"yes ( start )"]);
-            buttons.push([1,"no ( start anyways )"]);
-            resetScreen("Narrator","Hello, player! Would you like to start the adventure?",buttons);
-            break;
-        case 1:
-            buttons = [];
-            buttons.push([2,"wake up"]);
-            buttons.push([3,"keep sleeping"]);
-            resetScreen("Alarm clock","brr brr .. brr brr ..", buttons);
-            break;
-        case 2:
-            buttons = [];
-            buttons.push([3,"go back to sleep"]);
-            resetScreen("You","Ugh I have no energy to do anything today!",buttons);
-            break;
-        case 3:
-            buttons = [];
-            resetScreen("Narrator","And so your adventure ended with you sleeping for 5 days and dying of dehydration.",buttons);
-            break;
-    }    
-}
+
+/* scene0 is the initial starting scene */
+const scene0 = new Scene("Narrator" , "Hello player! Start the game?" , "scene0");
+scene0.add_button(["scene1" , "yes"]);
+scene0.switch_scene();
+create_scene(scene0);
+
+/* this is an example of how to create the next scene */
+/* scene1 on the left side is the variable and on the right side scene1 is the name of the scene
+that the progras recognises when creating a button */
+
+const scene1 = new Scene("Narrator","You woke up!","scene1");
+scene1.add_button(["scene2","go back to sleep"]);
+scene1.add_button(["scene3", "wash teeth"]);
+create_scene(scene1);
+
+const scene2 = new Scene("Narrator","You sleep for 5 days and die of dehydration","scene2");
+create_scene(scene2);
+
+const scene3 = new Scene("You","Eh I don't feel like washing my teeth");
+scene3.add_button(["scene2","go to sleep"]);
+create_scene(scene3);
