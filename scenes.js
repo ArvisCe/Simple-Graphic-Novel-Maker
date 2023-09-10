91e01e1f3081738bc9539a02ced399033396bec9
Arvis Ceirulis <arvis@Arviss-MacBook-Pro.local>
2023-09-10 17:42:16 +0300

    feat: created a better suited example project

--- scenes.js
+++ scenes.js
@@ -2,34 +2,24 @@ function switch_scene(scene){
     switch(scene){
         case 0:
             buttons = [];
-            buttons.push([1,"clickme"]);
-            buttons.push([2,"clickme"]);
-            buttons.push([3,"clickme"]);
-            resetScreen("Narrator","Hello this is the narrator! Switch to scene: ",buttons);
+            buttons.push([1,"yes ( start )"]);
+            buttons.push([1,"no ( start anyways )"]);
+            resetScreen("Narrator","Hello, player! Would you like to start the adventure?",buttons);
             break;
         case 1:
-            setScreenImage('image1.png');
             buttons = [];
-            buttons.push([1,"clickme"]);
-            buttons.push([2,"clickme"]);
-            buttons.push([3,"clickme"]);
-            resetScreen("Narrator","scene 1",buttons);
+            buttons.push([2,"wake up"]);
+            buttons.push([3,"keep sleeping"]);
+            resetScreen("Alarm clock","brr brr .. brr brr ..", buttons);
             break;
         case 2:
-            setScreenImage('image2.png');
             buttons = [];
-            buttons.push([1,"clickme"]);
-            buttons.push([2,"clickme"]);
-            buttons.push([3,"clickme"]);
-            resetScreen("Narrator","scene 2",buttons);
+            buttons.push([3,"go back to sleep"]);
+            resetScreen("You","Ugh I have no energy to do anything today!",buttons);
             break;
         case 3:
-            setScreenImage('image3.png');
             buttons = [];
-            buttons.push([1,"clickme"]);
-            buttons.push([2,"clickme"]);
-            buttons.push([3,"clickme"]);
-            resetScreen("Narrator","scene 3",buttons);
+            resetScreen("Narrator","And so your adventure ended with you sleeping for 5 days and dying of dehydration.",buttons);
             break;
     }    
 }
